0c476a3f0efd0e0442025d3f26ae6e3440f30185
Constantin Cristian <cch30c1987@gmail.com>
2019-03-09 19:18:27 +0200

    Create ro.js
    romanian translation for the tinymce plugin.

---
+++ plugins/codemirror/langs/ro.js
@@ -0,0 +1,8 @@
+tinymce.addI18n('en',{
+    'HTML source code': 'cod sursă HTML',
+    'Start search': 'Căutare',
+    'Find next': 'Găsește următorul',
+    'Find previous': 'Găsește anteriorul',
+    'Replace': 'Înlocuiește',
+    'Replace all': 'Înlocuiește tot'
+});
